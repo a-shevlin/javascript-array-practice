887c038b654ebe1a8f9cf6067f51d47235466b7e
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-06 15:22:02 -0700

    Add example of forEach loops for both numbers and strings.
    Co-authored-by: carolinecerussi <carolinecerussi@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -31,7 +31,47 @@ const originalArray = [1,2,3,4,5];
 let cloneArray = originalArray.slice(3);
 cloneArray.push(6);
 /* declaring a new array using the same elements but changing the copy use .slice() otherwise you change both arrays */
-/* use this instead of creating a pointer "let cloneArray = originalArray" */
+/* use this instead of creating a pointer -> "let cloneArray = originalArray" */
 
 console.log(months);
 console.log(modifiedMonths);
+
+originalArray.forEach(function(number) {
+  console.log(number * 2);
+});
+/* logs each element in originalArray to console after multiplying */
+let doubledArray = [];
+originalArray.forEach (function(element) {
+  doubledArray.push (element *2);
+});
+/* pushes the originalArray * 2 to doubledArray value */
+let sumArray = 0;
+originalArray.forEach(function(element) {
+  sumArray += element;
+});
+/* adds values from originalArray to each other and assigns to sumArray */
+
+const sibNames = ["Alex","Katie"];
+const rentNames = ["Mary","Marie"];
+let combineNames = sibNames.concat(rentNames);
+console.log (combineNames);
+
+combineNames.push("Finnley","Milo");
+console.log(combineNames);
+
+const reverseNames = combineNames.reverse();
+console.log(reverseNames);
+
+reverseNames[3] = "Michael";
+
+let thingsILike = "I like...";
+reverseNames.forEach(function(person) {
+  thingsILike = thingsILike.concat(" " + person + "!");
+});
+/*use a forEach .concat to add to a string. */
+
+
+reverseNames.forEach(function(person) {
+  $("#people").append(" " + person + "!");
+});
+/* use forEach .append to add to pre-existing #id */
